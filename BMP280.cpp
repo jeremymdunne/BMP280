6ef0d5be5187c42ca16e02231705bffad1163fd7
Jeremy Dunne <jeremymdunne@gmail.com>
2023-01-04 09:39:17 -0500

    ready to test

--- BMP280.cpp
+++ BMP280.cpp
@@ -79,10 +79,116 @@ BMP280_status_t BMP280::readPressData(byte* pressure){
     // read the pressure data 
     // does not run the compensation routine 
     select(); 
-    SPI.transfer(BMP280_PRESS_MSB); 
+    SPI.transfer(BMP280_PRESS_MSB|1<<7); 
     *pressure = SPI.transfer(0); 
     *(pressure + 1) = SPI.transfer(0); 
     *(pressure + 2) = SPI.transfer(0); 
     release(); 
     return BMP280_OK; // TODO implement check update thing 
+}
+
+BMP280_status_t BMP280::readTempData(byte* temp){
+    // read the temperature data 
+    // does not run the compensation routine
+    select(); 
+    SPI.transfer(BMP280_TEMP_MSB|1<<7); 
+    *temp = SPI.transfer(0); 
+    *(temp + 1) = SPI.transfer(0); 
+    *(temp + 2) = SPI.transfer(0); 
+    release(); 
+    return BMP280_OK; 
+}
+
+BMP280_status_t BMP280::readPressTempData(byte* pressure, byte* temp){
+    // perform a burst read of the temperature & pressure data
+    select(); 
+    SPI.transfer(BMP280_PRESS_MSB|1<<7); 
+    *pressure = SPI.transfer(0); 
+    *(pressure + 1) = SPI.transfer(0); 
+    *(pressure + 2) = SPI.transfer(0); 
+    *temp = SPI.transfer(0); 
+    *(temp + 1) = SPI.transfer(0); 
+    *(temp + 2) = SPI.transfer(0); 
+    release(); 
+    return BMP280_OK; 
+}
+
+float BMP280::compensatePressure(signed int pressure_data){ 
+    // compensate the pressure adc measurement 
+    // uses Bosch's method described in the datasheet
+    // requires the temperature compensation to be run prior
+    // TODO verify signed long is a 64 bit value 
+    signed long var1, var2, p; 
+    var1 = ((signed long)_t_fine) - 128000; 
+    var2 = var1 * var1 * (signed long)_calib_data.dig_P6; 
+    var2 = var2 + ((var1*(signed long)_calib_data.dig_P5)<<17); 
+    var2 = var2 + ((signed long)_calib_data.dig_P4<<35);
+    var1 = ((var1 * var1 * (signed long)_calib_data.dig_P3)>>8) + ((var1 * (signed long)_calib_data.dig_P2)<<12); 
+    var1 = (((((signed long)1)<<47)+var1))*((signed long)_calib_data.dig_P1)>>33; 
+    if(var1 == 0){
+        return 0; // div by 0 catch 
+    }
+    p = 1048576 - pressure_data; 
+    p = (((p<<31)-var2)*3125)/var1; 
+    var1 = (((signed long)_calib_data.dig_P9) * (p>>13) * (p>>13)) >> 25; 
+    var2 = (((signed long)_calib_data.dig_P8) *p) >> 19; 
+    p = ((p + var1 + var2) >> 8) + (((signed long)_calib_data.dig_P7)<<4); 
+    return (float)(p/256); 
+}
+
+float BMP280::compensateTemperature(signed int temperature_data){
+    // compensate the temperature adc measurement 
+    // uses Bosch's method described in the datasheet 
+    // TODO verify data type sizes 
+    signed int var1, var2, T; 
+    var1 = ((((temperature_data>>3) - ((signed int)_calib_data.dig_T1<<1))) * ((signed int)_calib_data.dig_T2)) >> 11; 
+    var2 = (((((temperature_data>>4) - ((signed int)_calib_data.dig_T1)) * ((temperature_data>>4) - ((signed int)_calib_data.dig_T1))) >> 12) * ((signed int)_calib_data.dig_T3)) >> 14; 
+    _t_fine = var1 + var2; 
+    T = (_t_fine * 5 + 128) >> 8; 
+    return (float)(T/100.0); 
+}
+
+float BMP280::getTemperature(){
+    // get the adc data then compensate it 
+    byte adc_temp[3]; 
+    _status = readTempData(adc_temp); 
+    // convert to a signed int ? TODO verify 
+    signed int temp_data = adc_temp[0] << 12 | adc_temp[1] << 4 | adc_temp[2] >> 4; 
+    return compensateTemperature(temp_data); 
+}
+
+float BMP280::getPressure(){
+    // because the pressure compensation depends on temperature data, both pressure and data need 
+    //  to be read. 
+    float press, temp; 
+    getPressureTemperature(&press, &temp); 
+    return press; 
+}
+
+BMP280_status_t BMP280::getPressureTemperature(float* pressure, float* temperature){
+    // read all the data 
+    // this is done so a continual burst read of all available data is performed 
+    byte temp_adc[3]; 
+    byte press_adc[3]; 
+    _status = readPressTempData(press_adc, temp_adc); 
+    // combine into signed data 
+    signed int temp_data = temp_adc[0] << 12 | temp_adc[1] << 4 | temp_adc[2] >> 4;
+    signed int press_data = press_adc[0] << 12 | press_adc[1] << 4 | press_adc[2] >> 4;
+    *temperature = compensateTemperature(temp_data); 
+    *pressure = compensatePressure(press_data); 
+    return _status; 
+}
+
+BMP280_status_t BMP280::readCalibrationData(){ 
+    // read in the calibration data 
+    int *ptr = (int*)&_calib_data; 
+    // struct is set up to do a straight read 
+    select(); 
+    // start reading at the starting address of the calib data 
+    SPI.transfer(BMP280_CALIB_25|1<<7); 
+    for(unsigned int i = 0; i < sizeof(_calib_data); i ++){
+        *(ptr + i) = SPI.transfer(0); 
+    }
+    release(); 
+    return BMP280_OK;
 }
--- BMP280.hpp
+++ BMP280.hpp
@@ -75,7 +75,7 @@
 
 // SPI Settings 
 #define BMP280_SPI_FREQ         10000000    // 10 MHz max speed 
-#define BMP280_SPI_MODE         SPI_MODE_0 
+#define BMP280_SPI_MODE         SPI_MODE_0  
 #define BMP280_SPI_DATA_ORDER   MSBFIRST 
 
 
@@ -85,34 +85,102 @@ typedef enum{
     BMP280_OK = 0, 
     BMP280_UNKNOWN_ID, 
     BMP280_COMM_FAIL
-} BMP280_status_t; 
+} BMP280_status_t; ///< standard return type for the BMP280 class 
 
-
+/**
+ * @brief interface library for the BMP280 barometer 
+ * 
+ */
 class BMP280{
 public: 
+    /**
+     * @brief initialize the barometer 
+     * 
+     * Begins communication with the barometer, checks the device ID, and gets the calibration 
+     *  data from the chip 
+     * 
+     * @param cs_pin chip select pin 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t init(int cs_pin); 
 
     // chip registers functions 
     
+    /**
+     * @brief read the ID of the chip 
+     * 
+     * @param id byte to store the ID in 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t readId(byte* id); 
 
+    /**
+     * @brief reset the barometer 
+     * 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t reset(); 
 
+    /**
+     * @brief read the status byte 
+     * 
+     * @param status byte to read the status into 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t readStatus(byte* status); 
 
+    /**
+     * @brief write the ctrl measurement register 
+     * 
+     * @param ctrl_meas data to write into the ctrl register 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t writeCtrlMeas(byte ctrl_meas); 
 
+    /**
+     * @brief write to the config register 
+     * 
+     * @param config data to write into the config register 
+     * @return BMP280_status_t 
+     */
     BMP280_status_t writeConfig(byte config); 
 
-    BMP280_status_t readPressData(byte* pressure); 
+    /**
+     * @brief get the temperature
+     * 
+     * reads temperature data from the barometer and runs the compensation routine 
+     * 
+     * @return float temperature in celcius 
+     */
+    float getTemperature(); 
 
-    BMP280_status_t readTempData(byte* temp); 
+    /**
+     * @brief get the pressure 
+     * 
+     * reads pressure data from the barometer and runs the compensation routine. Reads both the temperature and 
+     *  the pressure as data is required from the temperature to complete the pressure compensation
+     * 
+     * @return float pressure in pascals
+     */
+    float getPressure(); 
+
+    /**
+     * @brief get the pressure and temperature 
+     * 
+     * reads pressure and temperature data from the barometer and runs the compensation routines 
+     * 
+     * @param pressure pointer to store the pressure in, in Pascals 
+     * @param temperature pointer to store the temperature in, in celcius
+     * @return BMP280_status_t 
+     */
+    BMP280_status_t getPressureTemperature(float* pressure, float* temperature); 
 
 private: 
     // private variables
     int _cs_pin; 
     SPISettings _spi_settings = SPISettings(BMP280_SPI_FREQ, BMP280_SPI_DATA_ORDER, BMP280_SPI_MODE); 
     BMP280_status_t _status; 
+    signed int _t_fine; 
 
     // calibration data 
     struct BMP280CalibData{
@@ -129,27 +197,84 @@ private:
         signed short dig_P8; 
         signed short dig_P9; 
     }; 
-    BMP280CalibData _calib_data; 
-
-
-    
-
+    BMP280CalibData _calib_data; ///< structure to store the calibration data in 
 
     // private functions 
+    /**
+     * @brief read the pressure data from the barometer 
+     * 
+     * reads the 3 bytes storing the applicable 20 bits of pressure data. Data is just adc data, must be compensated. 
+     * 
+     * @param pressure buffer of size 3 to store the pressure data in. Data is 20 bits long, should be shifted the last 4 bits 
+     * @return BMP280_status_t 
+     */
+    BMP280_status_t readPressData(byte* pressure); 
+
+    /**
+     * @brief read the temperature data from the barometer 
+     * 
+     * reads the 3 bytes storing the applicable 20 bits of temperature data. Data is just adc data, must be compensated.
+     * 
+     * @param temp buffer of size 3 to store temperature data in. Data is 20 bits long, should be shifted by 4 bits 
+     * @return BMP280_status_t 
+     */
+    BMP280_status_t readTempData(byte* temp); 
+
+    /**
+     * @brief read the pressure and temperature data 
+     * 
+     * Performs a single burst read to get both the pressure and temperature data. Reads 6 bytes in total. 
+     * 
+     * @param pressure buffer of size 3 to store the pressure data in. Data is 20 bits long, should be shifted the last 4 bits 
+     * @param temp uffer of size 3 to store temperature data in. Data is 20 bits long, should be shifted by 4 bits 
+     * @return BMP280_status_t 
+     */
+    BMP280_status_t readPressTempData(byte* pressure, byte* temp); 
+
+    /**
+     * @brief compensates the raw pressure data 
+     * 
+     * uses the recommended procedure to compensate the pressure data to convert adc data into actual measurements. 
+     * requires the temperature compensation routine to be run first. 
+     * 
+     * @param pressure_data pressure adc data from the barometer 
+     * @return float pressure in pascals 
+     */
+    float compensatePressure(signed int pressure_data); 
+
+    /**
+     * @brief compensates the raw temperature data 
+     * 
+     * uses the recommended procedure to compensate the temperature data to convert adc data into measurements. 
+     * 
+     * @param temperature_data temperature adc data from the barometer 
+     * @return float temperature in celcius
+     */
+    float compensateTemperature(signed int temperature_data); 
 
     BMP280_status_t readCalibrationData(); 
 
+    /**
+     * @brief select the device on the SPI bus 
+     * 
+     * selects the barometer and starts the SPI transaction with correct chip settings 
+     * 
+     */
     void select(){
         digitalWrite(_cs_pin, LOW); 
         SPI.beginTransaction(_spi_settings); 
     }; 
 
+    /**
+     * @brief releases the device on the SPI bus 
+     * 
+     * releases the barometer and ends the SPI transaction 
+     * 
+     */
     void release(){
         digitalWrite(_cs_pin, HIGH); 
         SPI.endTransaction(); 
     }; 
-
-
 }; 
 
 #endif
